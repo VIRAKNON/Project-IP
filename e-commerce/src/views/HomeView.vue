edf6552f28d5c54d919cd707b5411e1e6791f0ba
VIRAKNON <VIRAKNON.sdeungviraknon@gmail.com>
2023-03-24 10:03:23 +0700

    TD3 Designs Home-page

---
+++ e-commerce/src/views/HomeView.vue
@@ -0,0 +1,292 @@
+<template>
+  <div class="container-fluid" style="background-color:#3664F4 ;">
+    <div class="d-flex justify-content-center">
+      <img src="../assets/images/homepageBanner.gif" alt="">
+    </div>
+    <div class="container-fluid d-flex justify-content-center">
+      <div class="content my-4 d-flex">
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/p1.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">Divano Roma Furniture - Modern Mid Century Accent Chair
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$60.00</del></span>
+              <span class="text-primary h5 px-3">$50.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/1003896_Egg-Chair-Red_grande.webp" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              The modern chair is something that perhaps we take for granted, but its evolution and development is a direct reflection of the social and economic status of society.
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$55.00</del></span>
+              <span class="text-primary h5 px-3">$45.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/71uzPcdBmnL._AC_SL1500_.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              LGESR Modern Dining Chair (2PCS) Dining Chair, Comfortable Upholstered Back Chair, Steel Frame Leg Makeup Chair, European-Style Simple Bedroom
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$40.00</del></span>
+              <span class="text-primary h5 px-3">$38.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/tv-ligne-roset-1.m.webp" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              TV Chair by Ligne Roset | Classicdesign.it
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$20.00</del></span>
+              <span class="text-primary h5 px-3">$18.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/0507b278-6f17-4616-9e18-497cd8ecc7f8.7718a0682c46648eb711b8d425b50863.jpeg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Fabric Swivel Accent Chair with Beech Wood Legs
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$25.00</del></span>
+              <span class="text-primary h5 px-3">$20.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid d-flex justify-content-center">
+      <div class="content my-4 d-flex">
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/ba3f66ee-3712-4d40-9dff-e049c5a58b38.d6f97090ec3274638b81ca9839cbb56c.jpeg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">            
+              William Mid Century Modern Furniture Tufted Futon Gray Fabric Couch Bed
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$160.00</del></span>
+              <span class="text-primary h5 px-3">$130.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/51KL34xC5kL.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Divano Roma Furniture Small Space Modern Sectional Sofa, Gray
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$200.00</del></span>
+              <span class="text-primary h5 px-3">$180.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/Kardiel-Woodrow-Lush-Mid-century-Modern-87-Sofa-Twill-ecc1b57f-bf36-4135-b3b2-a4f73ef31207.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Kardiel Woodrow Lush Mid-century Modern 87" Sofa, Twill
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$260.00</del></span>
+              <span class="text-primary h5 px-3">$240.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/Hdd90bdc66f954f0bb387b699040a7172m.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Luxury European Furniture American Sofa Set Modern Furniture French Cheap Living Room Single 3-seater Sofa - Buy American Sofa Set Modern
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$280.00</del></span>
+              <span class="text-primary h5 px-3">$260.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/WhatsApp-Image-2021-12-20-at-10.00.48-PM-e1640681501355.jpeg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Ultra Modern Sofa
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$360.00</del></span>
+              <span class="text-primary h5 px-3">$330.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid d-flex justify-content-center">
+      <div class="content my-4 d-flex">
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/HTB1HOR4XyHrK1Rjy0Flq6AsaFXaF.jpg_640x640Q90.jpg_.webp" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Buy Smart King Size Modern Bed with Integrated Massager in White Colour by Casa Moderna Online - Modern King Size Beds - Beds - Furniture - Pepperfry Product
+            </p>
+            <nav>
+              <span class="text-danger h5 px-2"><del>$3600.00</del></span>
+              <span class="text-primary h5 px-2">$3500.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/charcoal-modway-platform-beds-mod-6887-cha-64_1000.avif" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              MODWAY Reagan Charcoal Black Performance Velvet Frame Twin Platform Bed MOD-6887-CHA - The Home Depot
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$200.00</del></span>
+              <span class="text-primary h5 px-3">$185.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/envy_queen_bed_wood_black_1220x610_corner_with_mattress_1_1.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Modern Beds | Buy Contemporary Bed Online | Modani Furniture
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$260.00</del></span>
+              <span class="text-primary h5 px-3">$250.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/03a497d6f4ba4c5d90e9f59b82f4cf03.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Chesani Full Upholstered Bed
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$265.00</del></span>
+              <span class="text-primary h5 px-3">$240.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <img src="../assets/images/41WNgGgSg1L.jpg" alt="">
+          </div>
+          <div class="pro-info">
+            <p class="px-3 py-1">
+              Meridian Furniture Elly Collection Modern | Contemporary Velvet Upholstered Bed with Deep Button Tufting and Espresso Wood Legs, Queen, Pink
+            </p>
+            <nav>
+              <span class="text-danger h5 px-3"><del>$260.00</del></span>
+              <span class="text-primary h5 px-3">$250.00</span>
+              <span class="px-4"><button @click="$emit('checkout')" class="btn btn-sm btn-outline-success"><i class="fas fa-cart-arrow-down"></i></button></span>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-center py-4">
+      <button class="btn btn-success px-3 h5">More</button>
+    </div>
+    
+  </div>
+  <footer>
+    <div class="container-fluid" style="background-color:#252525 ;">
+        <div class="row">
+            <div class="col-4">
+                <div class="my-3">
+                    <a class="navbar-brand" href="#">
+                        <span><img src="../assets/images/snapsvg-logo.png" alt="logo" width="52px" ></span>
+                        <span class="h1 text-warning px-2">Modern Furnitures</span>
+                    </a>
+                </div>
+                <div class="">
+                    <nav>
+                        <a href="#"><i class="fab fa-google"></i></a>
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-4 d-flex justify-content-center align-item-center">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item "><span class="nav-link"><RouterLink to="/"><span>Home</span></RouterLink></span></li>
+                    <li class="nav-item "><span class="nav-link"><RouterLink to="/about"><span>About</span></RouterLink></span></li>
+                    <li class="nav-item "><span class="nav-link"><RouterLink to="/product"><span>Product</span></RouterLink></span></li>
+                </ul>
+            </div>
+            <div class="footer col-4">
+                <h4 class="text-light mt-3 py-2">Stay In Touch! Join our Newsletter</h4>
+                <a href="#"><i class="fab fa-cc-visa"></i></a>
+                <a style="color:#4265D6 ;" href="#" ><i class="fab fa-cc-amex"></i></a>
+                <a href="#"><i class="fab fa-cc-mastercard"></i></a>
+            </div>
+        </div>
+    </div>
+  </footer>
+</template>
+
+<style>
+@media (min-width: 1618px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    
+  }
+}
+</style>
